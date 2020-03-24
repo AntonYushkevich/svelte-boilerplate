74fb71b6121cfb1e11466ee6b86aded9729ffcd2
yushkevich <anton.yushkevich@timvero.com>
2020-03-24 14:07:40 +0300

    base config

---
+++ rollup.config.js
@@ -0,0 +1,77 @@
+import svelte from 'rollup-plugin-svelte';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
+import {terser} from 'rollup-plugin-terser';
+import sveltePreprocess from 'svelte-preprocess';
+import serve from 'rollup-plugin-serve';
+import babel from 'rollup-plugin-babel';
+import builtins from 'rollup-plugin-node-builtins';
+import includePaths from 'rollup-plugin-includepaths';
+import replace from "rollup-plugin-replace";
+
+const production = !process.env.ROLLUP_WATCH;
+const preprocess = sveltePreprocess({
+  scss: {
+    includePaths: ['src'],
+  },
+  postcss: {
+    plugins: [require('autoprefixer')],
+  },
+});
+
+export default {
+  input: 'src/main.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: 'public/build/bundle.js'
+  },
+  plugins: [
+    svelte({
+      // enable run-time checks when not in production
+      dev: !production,
+      // we'll extract any component CSS out into
+      // a separate file - better for performance
+      css: css => {
+        css.write('public/build/bundle.css');
+      },
+      preprocess,
+    }),
+    resolve({
+      browser: true,
+      dedupe: ['svelte'],
+      preferBuiltins: true,
+    }),
+    builtins(),
+    commonjs(),
+    !production && serve({
+      contentBase: 'public',
+      path: 'src',
+      open: true,
+      historyApiFallback: true,
+      port: 3000,
+      proxy: {
+        api: 'http://localhost:8080'
+      }
+    }),
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload('public'),
+    production && terser(),
+    babel({
+      exclude: [
+        'node_modules/**'
+      ]
+    }),
+    // resolve absolute import
+    includePaths({paths: ["./"], external: ['node_modules']}),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(production ? 'production' : 'development')
+    })
+  ],
+  watch: {
+    clearScreen: false
+  }
+};
---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+
+</script>
+
+<main>
+    <h1>Svelte app</h1>
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+        display: grid;
+        grid-template-areas: "aside header" "aside content";
+        grid-template-columns: minmax(80px, 280px) 1fr;
+    }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+</style>
